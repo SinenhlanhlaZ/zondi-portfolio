57ff39bfc2d3b2e2c4e33df86694596e860b4d86
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-11-27 14:50:14 +0200

    completed contact component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {BsInstagram} from 'react-icons/bs'
 
 const contact = () => {
   return (
@@ -10,23 +11,32 @@ const contact = () => {
       <h2> Contact me </h2>
 
       <div className="container contact__container">
-
-        <div className="contact__options">
+        
+        <div className="contact__options__container">
           <div className="contact__option">
             <MdOutlineEmail className="contact__option-icon"/>
             <h4> Email </h4>
             <h5> dev@sinezondi.com </h5>
           </div>
+
+          <div className="contact__option">
+            <BsInstagram className="contact__option-icon"/>
+            <h4> Instagram </h4>
+            <h5> sine.zondi </h5>
+          </div>
         </div>
 
-        <form action="">
-          <input type="text" name="name" placeholder="Full name" required/>
-          <input type="email" name="email" placeholder="Email address" required/>
-          <textarea name="message" rows="7" placeholder="Your message" required></textarea>
-          <button type="submit" className="btn btn-primary"> Send </button>
-        </form>
-
+        <div className="form__container">
+          <form action="" method="POST">
+            <input type="text" name="name" placeholder="Full name" required/>
+            <input type="email" name="email" placeholder="Email address" required/>
+            <textarea name="message" rows="7" placeholder="Your message" required></textarea>
+            <button type="submit" className="btn btn-primary"> Send </button>
+          </form>
+        </div>
       </div>
+      <br/>
+      <br/>
       <br/>
     </div>
   )
